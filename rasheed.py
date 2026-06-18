e9e35dd5441a1659aa3560590b4db5527ce48c76
rasheed-sr7 <abrasheed326@gmail.com>
2026-06-18 06:25:35 +0530

    first python code

--- rasheed.py
+++ rasheed.py
@@ -1,1 +1,4 @@
+
 print('Rasheed is boss')
+hi i am shaik abdul rasheed i am  devops engineer in the stalwart company 
+ i have 4 years of experience in the devops and the coding  i have hands on experience in the gitlab and the aws 
