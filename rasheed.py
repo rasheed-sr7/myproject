1b8d05272012d09f0f39e0e66837ffcdb08c2727
futureopselyon07-blip <futureopselyon07@gmail.com>
2026-06-20 15:09:41 +0530

    my firat commit

--- rasheed.py
+++ rasheed.py
@@ -1,4 +1,7 @@
+    
 
+this is a previlage that i am very exited to announce that i am learning new tools like the gitlab and the git also i am learning the most advancedd tools like the ci ad cd so i 
+  
 print('Rasheed is boss')
 hi i am shaik abdul rasheed i am  devops engineer in the stalwart company 
  i have 4 years of experience in the devops and the coding  i have hands on experience in the gitlab and the aws 
